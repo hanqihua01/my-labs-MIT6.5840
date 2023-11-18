71866983545b795eb5c7d9c255013c2e66a981fb
Qihua Han <qihua.han@icloud.com>
2023-11-18 22:14:22 +0100

    Finish worker

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -6,8 +6,10 @@ package mr
 // remember to capitalize all names.
 //
 
-import "os"
-import "strconv"
+import (
+	"os"
+	"strconv"
+)
 
 //
 // example to show how to declare the arguments
@@ -23,7 +25,21 @@ type ExampleReply struct {
 }
 
 // Add your RPC definitions here.
+type WorkerArgs struct {
+	FinishedMapTaskNumber    int // 完成的map任务编号
+	FinishedReduceTaskNumber int // 完成的reduce任务编号
+}
 
+type MasterReply struct {
+	TaskType int // 0: map, 1: reduce, 2: wait, 3: exit
+
+	NMap          int    // map任务的数量
+	MapTaskNumber int    // map任务的编号
+	MapFileName   string // map任务读取的文件名
+
+	NReduce          int // reduce任务的数量
+	ReduceTaskNumber int // reduce任务的编号
+}
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,33 +1,41 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
+	"sort"
+	"strconv"
+	"time"
+)
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
+// for sorting by key.
+type ByKey []KeyValue
+
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
 // main/mrworker.go calls this function.
-//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
@@ -36,13 +44,113 @@ func Worker(mapf func(string, string) []KeyValue,
 	// uncomment to send the Example RPC to the coordinator.
 	// CallExample()
 
+	for {
+		args := WorkerArgs{}
+		reply := MasterReply{}
+		ok := call("Coordinator.AllocateTask", &args, &reply)
+		if !ok || reply.TaskType == 3 {
+			break
+		}
+		if reply.TaskType == 0 { // map
+			intermediate := []KeyValue{}
+			// 读取文件，生成中间键值对
+			file, err := os.Open(reply.MapFileName)
+			if err != nil {
+				log.Fatalf("cannot open %v", reply.MapFileName)
+			}
+			content, err := ioutil.ReadAll(file)
+			if err != nil {
+				log.Fatalf("cannot read %v", reply.MapFileName)
+			}
+			file.Close()
+			kva := mapf(reply.MapFileName, string(content))
+			intermediate = append(intermediate, kva...)
+			// 将中间键值对按照hash值分桶
+			buckets := make([][]KeyValue, reply.NReduce)
+			for i := range buckets {
+				buckets[i] = []KeyValue{}
+			}
+			for _, kv := range intermediate {
+				buckets[ihash(kv.Key)%reply.NReduce] = append(buckets[ihash(kv.Key)%reply.NReduce], kv)
+			}
+			// 将桶中的键值对写入文件，每个桶一个文件
+			for i := range buckets {
+				oname := "mr-" + strconv.Itoa(reply.MapTaskNumber) + "-" + strconv.Itoa(i)
+				ofile, _ := ioutil.TempFile("", oname+"*")
+				enc := json.NewEncoder(ofile)
+				for _, kva := range buckets[i] {
+					err := enc.Encode(&kva)
+					if err != nil {
+						log.Fatalf("cannot write into %v", oname)
+					}
+				}
+				os.Rename(ofile.Name(), oname)
+				ofile.Close()
+			}
+			// 通知master完成map任务
+			finishArgs := WorkerArgs{reply.MapTaskNumber, -1}
+			finishReply := ExampleReply{}
+			call("Coordinator.FinishMapTask", &finishArgs, &finishReply)
+		} else if reply.TaskType == 1 { // reduce
+			intermediate := []KeyValue{}
+			// 读取文件，保存中间键值对
+			for i := 0; i < reply.NMap; i++ {
+				iname := "mr-" + strconv.Itoa(i) + "-" + strconv.Itoa(reply.ReduceTaskNumber)
+				file, err := os.Open(iname)
+				if err != nil {
+					log.Fatalf("cannot open %v", file)
+				}
+				dec := json.NewDecoder(file)
+				for {
+					var kv KeyValue
+					if err := dec.Decode(&kv); err != nil {
+						break
+					}
+					intermediate = append(intermediate, kv)
+				}
+				file.Close()
+			}
+			// 将中间键值对按照key排序
+			sort.Sort(ByKey(intermediate))
+			// 将相同key的value放在一起，调用reduce函数，将结果写入文件
+			oname := "mr-out-" + strconv.Itoa(reply.ReduceTaskNumber)
+			ofile, _ := ioutil.TempFile("", oname+"*")
+			i := 0
+			for i < len(intermediate) {
+				j := i + 1
+				for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+					j++
+				}
+				values := []string{}
+				for k := i; k < j; k++ {
+					values = append(values, intermediate[k].Value)
+				}
+				output := reducef(intermediate[i].Key, values)
+				fmt.Fprintf(ofile, "%v %v\n", intermediate[i].Key, output)
+				i = j
+			}
+			os.Rename(ofile.Name(), oname)
+			ofile.Close()
+			// 删除读取过的文件
+			for i := 0; i < reply.NMap; i++ {
+				iname := "mr-" + strconv.Itoa(i) + "-" + strconv.Itoa(reply.ReduceTaskNumber)
+				err := os.Remove(iname)
+				if err != nil {
+					log.Fatalf("cannot open delete" + iname)
+				}
+			}
+			// 通知master完成reduce任务
+			finishArgs := WorkerArgs{-1, reply.ReduceTaskNumber}
+			finishReply := ExampleReply{}
+			call("Coordinator.FinishReduceTask", &finishArgs, &finishReply)
+		}
+		time.Sleep(time.Second)
+	}
 }
 
-//
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func CallExample() {
 
 	// declare an argument structure.
@@ -67,11 +175,9 @@ func CallExample() {
 	}
 }
 
-//
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
